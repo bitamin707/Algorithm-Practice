9cbe7f528266fdb645de3505eaa4971abc998070
bitamin707 <bitamin707@naver.com>
2025-08-15 21:48:25 +0900

    BOJ_2579 : 계단 오르기

--- README.md
+++ README.md
@@ -19,6 +19,7 @@
 | 실버 3 | 1003 | 피보나치 함수 | [코드](./src/main/java/baekjoon/BOJ_1003.java) | [테스트](./src/test/java/baekjoon/BOJ1003Test.java) | [문제 링크](https://www.acmicpc.net/problem/1003) |
 | 실버 3 | 1463 | 1로 만들기 | [코드](./src/main/java/baekjoon/BOJ_1463.java) | [테스트](./src/test/java/baekjoon/BOJ1463Test.java) | [문제 링크](https://www.acmicpc.net/problem/1463) |
 | 실버 3 | 1966 | 프린터 큐 | [코드](./src/main/java/baekjoon/BOJ_1966.java) | [테스트](./src/test/java/baekjoon/BOJ1966Test.java) | [문제 링크](https://www.acmicpc.net/problem/1966) |
+| 실버 3 | 2579 | 계단 오르기 | [코드](./src/main/java/baekjoon/BOJ_2579.java) | [테스트](./src/test/java/baekjoon/BOJ2579Test.java) | [문제 링크](https://www.acmicpc.net/problem/2579) |
 | 실버 4 | 1018 | 체스판 다시 칠하기 | [코드](./src/main/java/baekjoon/BOJ_1018.java) | [테스트](./src/test/java/baekjoon/BOJ1018Test.java) | [문제 링크](https://www.acmicpc.net/problem/1018) |
 | 실버 4 | 10816 | 숫자 카드 2 | [코드](./src/main/java/baekjoon/BOJ_10816.java) | [테스트](./src/test/java/baekjoon/BOJ10816Test.java) | [문제 링크](https://www.acmicpc.net/problem/10816) |
 | 실버 4 | 10866 | 덱 | [코드](./src/main/java/baekjoon/BOJ_10866.java) | [테스트](./src/test/java/baekjoon/BOJ10866Test.java) | [문제 링크](https://www.acmicpc.net/problem/10866) |
---
+++ src/test/java/baekjoon/BOJ2579Test.java
@@ -0,0 +1,60 @@
+package baekjoon;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+@DisplayName("계단 오르기")
+class BOJ2579Test {
+
+    @Test
+    void testCase1_basicCase() throws IOException {
+        String input = """
+                6
+                10
+                20
+                15
+                25
+                10
+                20
+                """;
+        String expected = "75"; // 최적 경로: 10 + 20 + 25 + 20
+        assertEquals(expected, BOJ_2579.solve(input));
+    }
+
+    @Test
+    void testCase2_singleStep() throws IOException {
+        String input = """
+                1
+                50
+                """;
+        String expected = "50"; // 계단 하나만 있는 경우
+        assertEquals(expected, BOJ_2579.solve(input));
+    }
+
+    @Test
+    void testCase3_twoSteps() throws IOException {
+        String input = """
+                2
+                30
+                40
+                """;
+        String expected = "70"; // 두 계단 모두 밟기
+        assertEquals(expected, BOJ_2579.solve(input));
+    }
+
+    @Test
+    void testCase4_edgeCase() throws IOException {
+        String input = """
+                3
+                10
+                20
+                30
+                """;
+        String expected = "50"; // 20 + 30 경로가 최대
+        assertEquals(expected, BOJ_2579.solve(input));
+    }
+}
