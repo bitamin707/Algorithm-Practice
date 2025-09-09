41682dea8f1857e1a3fb7037fdf73beb9e9679b2
bitamin707 <bitamin707@naver.com>
2025-09-09 17:44:14 +0900

    BOJ_1389 : 케빈 베이컨의 6단계 법칙

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
 |--------|-----------|------|------|--------|---|
 | 실버 1 | 1080 | 행렬 | [코드](./src/main/java/baekjoon/BOJ_1080.java) | [테스트](./src/test/java/baekjoon/BOJ1080Test.java) | [문제 링크](https://www.acmicpc.net/problem/1080) |
 | 실버 1 | 11286 | 절댓값 힙 | [코드](./src/main/java/baekjoon/BOJ_11286.java) | [테스트](./src/test/java/baekjoon/BOJ11286Test.java) | [문제 링크](https://www.acmicpc.net/problem/11286) |
+| 실버 1 | 1389 | 케빈 베이컨의 6단계 법칙 | [코드](./src/main/java/baekjoon/BOJ_1389.java) | [테스트](./src/test/java/baekjoon/BOJ1389Test.java) | [문제 링크](https://www.acmicpc.net/problem/1389) |
 | 실버 1 | 2178 | 미로 탐색 | [코드](./src/main/java/baekjoon/BOJ_2178.java) | [테스트](./src/test/java/baekjoon/BOJ2178Test.java) | [문제 링크](https://www.acmicpc.net/problem/2178) |
 | 실버 2 | 1012 | 유기농 배추 | [코드](./src/main/java/baekjoon/BOJ_1012.java) | [테스트](./src/test/java/baekjoon/BOJ1012Test.java) | [문제 링크](https://www.acmicpc.net/problem/1012) |
 | 실버 2 | 11279 | 최대 힙 | [코드](./src/main/java/baekjoon/BOJ_11279.java) | [테스트](./src/test/java/baekjoon/BOJ11279Test.java) | [문제 링크](https://www.acmicpc.net/problem/11279) |
---
+++ src/test/java/baekjoon/BOJ1389Test.java
@@ -0,0 +1,113 @@
+package baekjoon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+class BOJ1389Test {
+
+    @Test
+    void testCase1_example() throws IOException {
+        String input = """
+                5 5
+                1 3
+                1 4
+                2 3
+                3 4
+                4 5
+                """;
+        String expected = "3";
+        assertEquals(expected, BOJ_1389.solve(input).strip());
+    }
+
+    @Test
+    void testCase2_simpleCase() throws IOException {
+        String input = """
+                4 4
+                1 2
+                2 3
+                3 4
+                1 4
+                """;
+        String expected = "1";
+        assertEquals(expected, BOJ_1389.solve(input).strip());
+    }
+
+    @Test
+    void testCase3_linearGraph() throws IOException {
+        String input = """
+                4 3
+                1 2
+                2 3
+                3 4
+                """;
+        String expected = "2";
+        assertEquals(expected, BOJ_1389.solve(input).strip());
+    }
+
+    @Test
+    void testCase4_starGraph() throws IOException {
+        String input = """
+                5 4
+                1 2
+                1 3
+                1 4
+                1 5
+                """;
+        String expected = "1";
+        assertEquals(expected, BOJ_1389.solve(input).strip());
+    }
+
+    @Test
+    void testCase5_smallGraph() throws IOException {
+        String input = """
+                3 2
+                1 2
+                2 3
+                """;
+        String expected = "2";
+        assertEquals(expected, BOJ_1389.solve(input).strip());
+    }
+
+    @Test
+    void testCase6_completeGraph() throws IOException {
+        String input = """
+                4 6
+                1 2
+                1 3
+                1 4
+                2 3
+                2 4
+                3 4
+                """;
+        String expected = "1";
+        assertEquals(expected, BOJ_1389.solve(input).strip());
+    }
+
+    @Test
+    void testCase7_twoConnectedComponents() throws IOException {
+        String input = """
+                6 4
+                1 2
+                2 3
+                4 5
+                5 6
+                """;
+        String expected = "2";
+        assertEquals(expected, BOJ_1389.solve(input).strip());
+    }
+
+    @Test
+    void testCase8_minNumberPreference() throws IOException {
+        String input = """
+                3 3
+                1 2
+                2 3
+                1 3
+                """;
+        String expected = "1";
+        assertEquals(expected, BOJ_1389.solve(input).strip());
+    }
+}
