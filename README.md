ae9626b8dc8b6be51acd0c2967ae8da3bd27c985
bitamin707 <bitamin707@naver.com>
2025-09-30 18:00:09 +0900

    BOJ_15654 : N과 M (5)

--- README.md
+++ README.md
@@ -56,6 +56,7 @@
 | 실버 3 | 15649 | N과 M (1) | [코드](./src/main/java/baekjoon/BOJ_15649.java) | [테스트](./src/test/java/baekjoon/BOJ15649Test.java) | [문제 링크](https://www.acmicpc.net/problem/15649) |
 | 실버 3 | 15650 | N과 M (2) | [코드](./src/main/java/baekjoon/BOJ_15650.java) | [테스트](./src/test/java/baekjoon/BOJ15650Test.java) | [문제 링크](https://www.acmicpc.net/problem/15650) |
 | 실버 3 | 15652 | N과 M (4) | [코드](./src/main/java/baekjoon/BOJ_15652.java) | [테스트](./src/test/java/baekjoon/BOJ15652Test.java) | [문제 링크](https://www.acmicpc.net/problem/15652) |
+| 실버 3 | 15654 | N과 M (5) | [코드](./src/main/java/baekjoon/BOJ_15654.java) | [테스트](./src/test/java/baekjoon/BOJ15654Test.java) | [문제 링크](https://www.acmicpc.net/problem/15654) |
 | 실버 3 | 1966 | 프린터 큐 | [코드](./src/main/java/baekjoon/BOJ_1966.java) | [테스트](./src/test/java/baekjoon/BOJ1966Test.java) | [문제 링크](https://www.acmicpc.net/problem/1966) |
 | 실버 3 | 2579 | 계단 오르기 | [코드](./src/main/java/baekjoon/BOJ_2579.java) | [테스트](./src/test/java/baekjoon/BOJ2579Test.java) | [문제 링크](https://www.acmicpc.net/problem/2579) |
 | 실버 3 | 2606 | 바이러스 | [코드](./src/main/java/baekjoon/BOJ_2606.java) | [테스트](./src/test/java/baekjoon/BOJ2606Test.java) | [문제 링크](https://www.acmicpc.net/problem/2606) |
---
+++ src/test/java/baekjoon/BOJ15654Test.java
@@ -0,0 +1,190 @@
+package baekjoon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+class BOJ15654Test {
+
+    @Test
+    void testCase1_example1() throws IOException {
+        String input = """
+                3 1
+                4 5 2
+                """;
+        String expected = """
+                2
+                4
+                5
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+
+    @Test
+    void testCase2_example2() throws IOException {
+        String input = """
+                4 2
+                9 8 7 1
+                """;
+        String expected = """
+                1 7
+                1 8
+                1 9
+                7 1
+                7 8
+                7 9
+                8 1
+                8 7
+                8 9
+                9 1
+                9 7
+                9 8
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+
+    @Test
+    void testCase3_example3() throws IOException {
+        String input = """
+                4 4
+                1231 1232 1233 1234
+                """;
+        String expected = """
+                1231 1232 1233 1234
+                1231 1232 1234 1233
+                1231 1233 1232 1234
+                1231 1233 1234 1232
+                1231 1234 1232 1233
+                1231 1234 1233 1232
+                1232 1231 1233 1234
+                1232 1231 1234 1233
+                1232 1233 1231 1234
+                1232 1233 1234 1231
+                1232 1234 1231 1233
+                1232 1234 1233 1231
+                1233 1231 1232 1234
+                1233 1231 1234 1232
+                1233 1232 1231 1234
+                1233 1232 1234 1231
+                1233 1234 1231 1232
+                1233 1234 1232 1231
+                1234 1231 1232 1233
+                1234 1231 1233 1232
+                1234 1232 1231 1233
+                1234 1232 1233 1231
+                1234 1233 1231 1232
+                1234 1233 1232 1231
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+
+    @Test
+    void testCase4_minimumCase() throws IOException {
+        String input = """
+                1 1
+                10
+                """;
+        String expected = """
+                10
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+
+    @Test
+    void testCase5_twoElements() throws IOException {
+        String input = """
+                2 1
+                5 3
+                """;
+        String expected = """
+                3
+                5
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+
+    @Test
+    void testCase6_twoTwo() throws IOException {
+        String input = """
+                2 2
+                10 20
+                """;
+        String expected = """
+                10 20
+                20 10
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+
+    @Test
+    void testCase7_unsortedInput() throws IOException {
+        String input = """
+                3 2
+                30 10 20
+                """;
+        String expected = """
+                10 20
+                10 30
+                20 10
+                20 30
+                30 10
+                30 20
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+
+    @Test
+    void testCase8_largeNumbers() throws IOException {
+        String input = """
+                3 2
+                9999 1000 5000
+                """;
+        String expected = """
+                1000 5000
+                1000 9999
+                5000 1000
+                5000 9999
+                9999 1000
+                9999 5000
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+
+    @Test
+    void testCase9_maxSize() throws IOException {
+        String input = """
+                8 1
+                8 7 6 5 4 3 2 1
+                """;
+        String expected = """
+                1
+                2
+                3
+                4
+                5
+                6
+                7
+                8
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+
+    @Test
+    void testCase10_threeTwo() throws IOException {
+        String input = """
+                3 3
+                100 200 300
+                """;
+        String expected = """
+                100 200 300
+                100 300 200
+                200 100 300
+                200 300 100
+                300 100 200
+                300 200 100
+                """;
+        assertEquals(expected, BOJ_15654.solve(input).strip());
+    }
+}
